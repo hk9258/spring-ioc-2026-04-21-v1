e389ebe9fb80435774198113a39f54b0ad168526
hk9258 <jhg9258@gmail.com>
2026-04-24 14:18:27 +0900

    t1 - ApplicationContext 객체 생성 (Green)

--- src/main/java/com/ll/framework/ioc/ApplicationContext.java
+++ src/main/java/com/ll/framework/ioc/ApplicationContext.java
@@ -1,11 +1,11 @@
 package com.ll.framework.ioc;
 
 public class ApplicationContext {
-    public ApplicationContext() {
 
+    public ApplicationContext() {
     }
 
     public <T> T genBean(String beanName) {
-        return (T) null;
+        return null;
     }
 }
