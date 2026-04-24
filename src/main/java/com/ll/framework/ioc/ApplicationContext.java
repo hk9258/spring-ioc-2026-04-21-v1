2e586e5355b3b0b068d4e5c76e42fe5451dd47b4
hk9258 <jhg9258@gmail.com>
2026-04-24 15:04:07 +0900

    t6 - testFacadePostService 의존성 주입 성공 (Green)

--- src/main/java/com/ll/framework/ioc/ApplicationContext.java
+++ src/main/java/com/ll/framework/ioc/ApplicationContext.java
@@ -1,16 +1,22 @@
 package com.ll.framework.ioc;
 
 import com.ll.domain.testPost.testPost.repository.TestPostRepository;
+import com.ll.domain.testPost.testPost.service.TestFacadePostService;
 import com.ll.domain.testPost.testPost.service.TestPostService;
 
 public class ApplicationContext {
 
     private final TestPostRepository testPostRepository;
     private final TestPostService testPostService;
+    private final TestFacadePostService testFacadePostService;
 
     public ApplicationContext() {
         testPostRepository = new TestPostRepository();
         testPostService = new TestPostService(testPostRepository);
+
+        // ✅ 추가
+        testFacadePostService =
+                new TestFacadePostService(testPostService, testPostRepository);
     }
 
     public <T> T genBean(String beanName) {
@@ -20,6 +26,10 @@ public class ApplicationContext {
 
         if (beanName.equals("testPostRepository")) {
             return (T) testPostRepository;
+        }
+
+        if (beanName.equals("testFacadePostService")) {
+            return (T) testFacadePostService;
         }
 
         return null;
