b65ff83c91d78c46756db53a3d28528760282f50
hk9258 <jhg9258@gmail.com>
2026-04-24 14:27:14 +0900

    t3 - testPostService 싱글톤 검증 실패 (Red)

--- src/test/java/com/ll/framework/ioc/ApplicationContextTest.java
+++ src/test/java/com/ll/framework/ioc/ApplicationContextTest.java
@@ -32,4 +32,18 @@ public class ApplicationContextTest {
         assertThat(testPostService).isNotNull();
     }
 
+    @Test
+    @DisplayName("testPostService 빈을 다시 얻기, 싱글톤이어야 함")
+    public void t3() {
+        TestPostService testPostService1 = applicationContext
+                .genBean("testPostService");
+
+        TestPostService testPostService2 = applicationContext
+                .genBean("testPostService");
+
+        assertThat(testPostService1).isNotNull();
+        assertThat(testPostService2).isNotNull();
+        assertThat(testPostService1).isSameAs(testPostService2);
+    }
+
 }
